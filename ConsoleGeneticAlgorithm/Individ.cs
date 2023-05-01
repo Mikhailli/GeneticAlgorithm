e7dd25479051769b5f1f6e8d7cb6116c6da043a8
Mikhail <babichev.mikhail.s@gmail.com>
2023-05-01 10:39:38 +0700

    work in console

--- ConsoleGeneticAlgorithm/Individ.cs
+++ ConsoleGeneticAlgorithm/Individ.cs
@@ -1,7 +1,126 @@
-﻿namespace ConsoleGeneticAlgorithm
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleGeneticAlgorithm;
+
+// Один индивид из популяции
+public class Individ
 {
-    public class Individ
-    {
+    private static readonly Random Random = new ();
         
+    // Границы поиска минимума
+    private readonly double _start;
+    private readonly double _end;
+
+    private readonly int _mutationSteps;
+
+    private readonly int _countOfComponents;
+
+    public readonly List<double> Components = new ();
+    
+    public double Score;
+
+    public readonly Func<List<double>, double> CalculateFunction;
+        
+    public Individ(double start, double end, int mutationSteps, int countOfComponents, Func<List<double>, double> calculateFunction)
+    {
+        _start = start;
+        _end = end;
+        
+        _mutationSteps = mutationSteps;
+
+        _countOfComponents = countOfComponents;
+
+        for (var i = 0; i < countOfComponents; i++)
+        {
+            Components.Add(GetRandomDoubleFromRange(start, end));
+        }
+
+        CalculateFunction = calculateFunction;
+        
+        Score = CalculateFunction(Components);
     }
+
+    // Функция мутации индивида
+    
+    public void Mutate(double mutateChance, int numberOfLife)
+    {
+        if (Random.NextDouble() <= mutateChance)
+        {
+            for (var i = 0; i < _countOfComponents; i++)
+            {
+                var component = MutateComponent(Components[i], numberOfLife);
+                Components[i] = component;
+            }
+        }
+    }
+    
+    private double MutateComponent(double component, int numberOfLife)
+    {
+        var deltaX = 0d;
+
+        for (var i = 0; i < _mutationSteps; i++)
+        {
+            if (Random.NextDouble() < 1d / _mutationSteps)
+            {
+                deltaX += 1d / Math.Pow(2, i);
+            }
+        }
+
+        if (Random.NextDouble() > 0.5)
+        {
+            deltaX *= _end;
+        }
+        else
+        {
+            deltaX *= _start;
+        }
+
+        component += deltaX;
+        ChangeInvalidComponent(ref component);
+        
+        return component;
+    }
+
+    private void ChangeInvalidComponent(ref double component)
+    {
+        if (component < _start)
+        {
+            component = _start;
+        }
+
+        if (component > _end)
+        {
+            component = _end;
+        }
+    }
+
+    private static double GetRandomDoubleFromRange(double start, double end)
+    {
+        return Random.NextDouble() * (end - start) + start;
+    }
+
+    /*public double CalculateFunction()
+    {
+        /*Score = -(X1 * Math.Sin(Math.Sqrt(Math.Abs(X1))) +
+                  X2 * Math.Sin(Math.Sqrt(Math.Abs(X2))) +
+                  X3 * Math.Sin(Math.Sqrt(Math.Abs(X3))) +
+                  X4 * Math.Sin(Math.Sqrt(Math.Abs(X4))) +
+                  X5 * Math.Sin(Math.Sqrt(Math.Abs(X5))) +
+                  X6 * Math.Sin(Math.Sqrt(Math.Abs(X6))) +
+                  X7 * Math.Sin(Math.Sqrt(Math.Abs(X7))) +
+                  X8 * Math.Sin(Math.Sqrt(Math.Abs(X8))) +
+                  X9 * Math.Sin(Math.Sqrt(Math.Abs(X9))) +
+                  X10 * Math.Sin(Math.Sqrt(Math.Abs(X10))));#1#
+        var sum = 1d;
+        var r = 1d;
+        for (var i = 0; i < _countOfComponents; i++)
+        {
+            sum += Math.Pow(Components[i], 2) / 4000;
+            r *= Math.Cos(Components[i] / Math.Sqrt(i + 1));
+        }
+
+        Score = sum - r;
+        return Score;
+    }*/
 }
